c2dbcdba1b2b08768244ff8380d235f537cf2c57
sstetch <130715050+sstetch@users.noreply.github.com>
2023-05-14 21:05:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,17 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "1404114574"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EE_47")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EE_20")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TXNX5")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/its_stetch")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1404114574").split()))
 
 
 FAILED = "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg"
