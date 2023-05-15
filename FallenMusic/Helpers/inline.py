20ed9590cacefc472688db0d089f996f943fe46a
sstetch <130715050+sstetch@users.noreply.github.com>
2023-05-15 18:42:51 +0300

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -33,10 +33,10 @@ close_key = InlineKeyboardMarkup(
 buttons = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="▶️", callback_data="resume_cb"),
-            InlineKeyboardButton(text="⏸", callback_data="pause_cb"),
-            InlineKeyboardButton(text="⏭️", callback_data="skip_cb"),
-            InlineKeyboardButton(text="⏹", callback_data="end_cb"),
+            InlineKeyboardButton(text="كمل", callback_data="resume_cb"),
+            InlineKeyboardButton(text="وقف مؤقتا", callback_data="pause_cb"),
+            InlineKeyboardButton(text="تخطي", callback_data="skip_cb"),
+            InlineKeyboardButton(text="ايقاف", callback_data="end_cb"),
         ]
     ]
 )
